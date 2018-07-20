a428826d8e9d5be88660d38f17f00567c1f15f37
Paul McCarthy <pauldmccarthy@gmail.com>
2018-07-20 12:29:29 +0100

    Don't know

--- testpkg/test_lib.py
+++ testpkg/test_lib.py
@@ -6,7 +6,7 @@ import os.path as op
 from . import lib
 
 
-def test_thing():
+def _test_thing():
 
     with lib.testdir() as testdir:
 
@@ -18,3 +18,19 @@ def test_thing():
 
         for d in dims:
             lib.make_random_image(fname, dims=d)
+
+
+
+def test_01(): _test_thing()
+def test_02(): _test_thing()
+def test_03(): _test_thing()
+def test_04(): _test_thing()
+def test_05(): _test_thing()
+def test_06(): _test_thing()
+def test_07(): _test_thing()
+def test_08(): _test_thing()
+def test_09(): _test_thing()
+def test_10(): _test_thing()
+def test_11(): _test_thing()
+def test_12(): _test_thing()
+def test_13(): _test_thing()
